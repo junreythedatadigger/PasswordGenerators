f1357c72dd102e493db644d264f3ec71eb08af00
junreythedatadigger <junrey.o.ansing@gmail.com>
2024-12-15 21:53:09 +0800

    Implemented input the paragraph to generate password

--- generate_password_from_paragraph.py
+++ generate_password_from_paragraph.py
@@ -1,1 +1,10 @@
-print("This code with generate password for paragraph.")
+# print("This code with generate password for paragraph.")
+
+# text = "Share in suffering as a good soldier of Christ Jesus. No soldier gets entangled in civilian pursuits, since his aim is to please the one who enlisted him."
+text = input("Enter paragraph: ")
+password = ""
+for word in text.split():
+    # print(word)
+    password += word[0]
+
+print(password)
